5b97d7b4fce7cf27ba520850191b94e17b10dc56
yomnaafifi <yomnaaafifi162@gmail.com>
2024-05-05 20:00:03 +0300

    adding base user serializer to auth

--- driver/serializer.py
+++ driver/serializer.py
@@ -1,31 +1,6 @@
 from rest_framework.serializers import ModelSerializer
 from driver.models import Driver
-from authentication.models import CustomUser
-
-
-class BaseUserSerializer(ModelSerializer):
-    def create(self, validated_data):
-        user = CustomUser.objects.create(
-            email=validated_data["email"],
-            first_name=validated_data["first_name"],
-            last_name=validated_data["last_name"],
-            birthdate=validated_data["birthdate"],
-            phone_no=validated_data["phone_no"],
-        )
-        user.set_password(validated_data["password"])
-        user.save()
-        return user
-
-    class Meta:
-        model = CustomUser
-        fields = [
-            "email",
-            "password",
-            "first_name",
-            "last_name",
-            "birthdate",
-            "phone_no",
-        ]
+from authentication.serializers import BaseUserSerializer
 
 
 class DriverUserSerializer(ModelSerializer):
