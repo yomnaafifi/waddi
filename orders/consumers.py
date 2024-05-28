8d5b224fc63ab0dea6d0d2d98d20e618e72984f4
yomnaafifi <yomnaaafifi162@gmail.com>
2024-05-28 19:07:33 +0300

    initial websocket

--- orders/consumers.py
+++ orders/consumers.py
@@ -1,4 +1,35 @@
 import json
 
 from channels.generic.websocket import AsyncWebsocketConsumer
-from asgiref.sync import sync_to_async
+from asgiref.sync import sync_to_async, async_to_sync
+
+
+class DriverConsumer(AsyncWebsocketConsumer):
+    def connect(self):
+        self.driver_group_name = (
+            "drivers"  # add city field to driver model and it should be the group name
+        )
+
+        self.accept()
+
+        async_to_sync(self.channel_layer.group_add)(
+            self.driver_group_name, self.channel_name
+        )
+
+    def disconnect(self, close_code):
+        async_to_sync(self.channel_layer.group_discard)(
+            self.driver_group_name, self.channel_name
+        )
+
+    def receive(self, text_data=None, bytes_data=None):
+        text_data_json = json.loads(text_data)
+        message = text_data_json["message"]
+
+        task_id = "12345"
+        async_to_sync(self.channel_layer.group_send)(
+            self.driver_group_name,
+            {
+                "type": "task_id",
+                "task_id": task_id,
+            },
+        )
