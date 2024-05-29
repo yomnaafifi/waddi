d1716dccd30578772f93152fab83c8bea988251e
yomnaafifi <yomnaaafifi162@gmail.com>
2024-05-29 03:14:53 +0300

    handle task acceptance

--- orders/consumers.py
+++ orders/consumers.py
@@ -10,7 +10,7 @@ from . import serializers
 class DriverConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.driver_group_name = (
-            self.driver_city
+            self.driver_city  # should use sth like scope but not sure yet
         )  # add city field to driver model and it should be the group name
         # how do i only add the drivers to the correct group based on the city
 
@@ -29,4 +29,27 @@ class DriverConsumer(AsyncWebsocketConsumer):
 
         await self.channel_layer.group_send(
             self.group_name, {"type": "order_message", "message": message}
+        )
+
+    async def handle_accept_task(self, data):
+        order_data = data("message")
+        driver_id = self.scope["user"].id
+
+        await self.save_order_data(order_data, driver_id)
+
+        await self.send(  # this is notifying the driver
+            text_data=json.dumps({"message": "Task accepted and assigned."})
+        )
+
+    @sync_to_async
+    def save_order_data(self, order_data, driver_id):
+        order = Orders.objects.create(
+            id=order_data.get("id"),
+            defaults={
+                "driver_id": driver_id,
+                "customer": order_data.get("customer"),
+                "order_state": order_data.get(
+                    "order_state"
+                ),  # will test this data first then add the rest of it
+            },
         )
