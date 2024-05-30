4582c108111cf27a924eb6ccbacfa971c159b2ca
ixbahy <ixbahy@gmail.com>
2024-05-31 01:12:01 +0300

    add task describtion

--- orders/consumers.py
+++ orders/consumers.py
@@ -27,9 +27,10 @@ class DriverConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         data = json.loads(text_data)
         message = data["message"]
+        type_ = data["type"]
 
         await self.channel_layer.group_send(
-            self.driver_group_name, {"type": "order_message", "message": message}
+            self.driver_group_name, {"type": type_, "message": message}
         )
 
     async def order_message(self, event):
@@ -38,6 +39,12 @@ class DriverConsumer(AsyncWebsocketConsumer):
         await self.send(text_data=json.dumps({"message": message}))
 
     async def handle_accept_task(self, data):
+        # here the driver will accept the task and the data will be sent to the server
+        # here the driver should send the id of the order he accepted
+        # and the server should update the order state to assigned with the driver to be the id of the user
+        # and then send id of the accepted order so that the front should remove it from all the drivers subscribed to the socket in the app UI
+        # the message will be the id of the order and the status to order-assigned
+        # so no other driver can send a another request to the same order from the UI
         order_data = data("message")
         driver_id = self.scope["user"].id
 
@@ -49,6 +56,33 @@ class DriverConsumer(AsyncWebsocketConsumer):
 
     @sync_to_async
     def save_order_data(self, order_data, driver_id):
+        # here you should create the order object from the data the user will send
+        # then save it to the database
+        # the schema of data the user will return
+        # the data will be :
+        # {
+        #     customer_id,
+        #     order_state:"default is unassigned",
+        #     pickup_location: {latitude, longitude},
+        #     dropoff_location: {latitude, longitude},
+        #     order_notes,
+        #     type(types): (plastic&rubber, appliances, glass, wood, food, furniture, multiple commodities),
+        #     commodity_image:optional,
+        #     chosen_truck,:(range from 1 to 4),
+        #     pickup_date,
+        #     pickup_time,
+        #     need_packing,
+        #     need_labor,
+        #     order_state:(unassigned, assigned, pickup, delivered, confirmed)
+
+        # }
+
+        ############################################################
+
+        # then u create the data and validarate it with a serializer and save it
+        # order = serializer.data
+        # then u publish to all subscribers the order data "not all fields just the required" and the type of message will be order_request
+
         order = Orders.objects.create(
             id=order_data.get("id"),
             defaults={
--- orders/models.py
+++ orders/models.py
@@ -13,6 +13,31 @@ class Location(models.Model):
         db_table = "location"
 
 
+types = {
+    ("plastic&rubber", "Plastic and Rubber"),
+    ("appliances", "Appliances"),
+    ("glass", "Glass"),
+    ("wood", "Wood"),
+    ("food", "Food"),
+    ("furniture", "Furinture"),
+    ("multiple commodities", "Multiple Commodities "),
+}
+
+truck_types = {
+    (1, "jumbo box 2700 kg"),
+    (2, "jumbo box 5200 kg"),
+    (3, "jumbo box 5200 kg"),
+    (4, "tricycle 2700 kg"),
+}
+order_states = {
+    ("unassigned", "Unassigned"),
+    ("assigned", "Assigned"),
+    ("pickup", "Pickup"),
+    ("delivered", "Delivered"),
+    ("confirmed", "Confirmed"),
+}
+
+
 class Orders(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     driver = models.ForeignKey(Driver, on_delete=models.CASCADE, null=True)
@@ -25,35 +50,13 @@ class Orders(models.Model):
     order_notes = models.CharField(max_length=255, blank=True, null=True)
     date_created = models.DateField(auto_now_add=True)
     time_created = models.TimeField(default=now)
-    types = {
-        ("plastic&rubber", "Plastic and Rubber"),
-        ("appliances", "Appliances"),
-        ("glass", "Glass"),
-        ("wood", "Wood"),
-        ("food", "Food"),
-        ("furniture", "Furinture"),
-        ("multiple commodities", "Multiple Commodities "),
-    }
     type = models.CharField(max_length=200, choices=types)
     commodity_image = models.ImageField(upload_to=None, null=True)
-    truck_types = {
-        (1, "jumbo box 2700 kg"),
-        (2, "jumbo box 5200 kg"),
-        (3, "jumbo box 5200 kg"),
-        (4, "tricycle 2700 kg"),
-    }
     chosen_truck = models.CharField(max_length=100, choices=truck_types, null=True)
     pickup_date = models.DateField(null=True)
     pickup_time = models.TimeField(null=True)
     need_packing = models.BooleanField()
     need_labor = models.BooleanField()
-    order_states = {
-        ("unassigned", "Unassigned"),
-        ("assigned", "Assigned"),
-        ("pickup", "Pickup"),
-        ("delivered", "Delivered"),
-        ("confirmed", "Confirmed"),
-    }
     order_state = models.CharField(
         max_length=100, choices=order_states, default="unassigned"
     )
