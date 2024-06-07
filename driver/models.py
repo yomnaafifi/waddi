56bd9acd7376f53d24dae06df0c98cbfb94a4ac7
yomnaafifi <yomnaaafifi162@gmail.com>
2024-06-07 13:02:52 +0300

    add car and license fields to driver model

--- driver/models.py
+++ driver/models.py
@@ -9,6 +9,30 @@ def generate_random_rating():
     return round(random.uniform(1.0, 5.0), 1)
 
 
+truck_types = [
+    "Ford F-150",
+    "Chevrolet Silverado",
+    "Ram 1500",
+    "Toyota Tundra",
+    "GMC Sierra",
+    "Nissan Titan",
+    "Honda Ridgeline",
+    "Jeep Gladiator",
+    "Ford Ranger",
+    "Chevrolet Colorado",
+]
+
+truck_type = random.choice(truck_types)
+
+
+def generate_random_license():
+    numbers = [str(random.randint(1, 9)) for _ in range(3)]
+    arabic_letters = "ابتثجحخدذرزسشصضطظعغفقكلمنهوي"
+    letters = [random.choice(arabic_letters) for _ in range(3)]
+    result = "".join(numbers) + " " + "".join(letters)
+    return result
+
+
 class Driver(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, primary_key=True)
     car_license = models.ImageField(
@@ -18,3 +42,7 @@ class Driver(models.Model):
     driver_license = models.ImageField(upload_to="images/", null=True)
     is_online = models.BooleanField(default=False)
     rating = models.FloatField(default=generate_random_rating)
+    truck = models.CharField(max_length=100, default=truck_type)
+    actual_car_license = models.CharField(
+        max_length=100, default=generate_random_license
+    )
