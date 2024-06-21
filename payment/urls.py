4a7ad9b2331bbba1598bab4d4e820b7b80bb906a
yomnaafifi <yomnaaafifi162@gmail.com>
2024-06-21 18:43:44 +0300

    add driver earnings api

--- payment/urls.py
+++ payment/urls.py
@@ -3,5 +3,14 @@ from . import views
 
 urlpatterns = [
     path("createtx/", views.createtx.as_view(), name="create_tx"),
-    path("drivertxhistory/", views.drivertxhistory.as_view(), name="driver_tx_history"),
+    path(
+        "drivertxhistory/<str:period>/",
+        views.drivertxhistory.as_view(),
+        name="driver_tx_history",
+    ),
+    path(
+        "driverearnings/<str:period>/",
+        views.driverearnings.as_view(),
+        name="driver_earnings",
+    ),
 ]
--- payment/views.py
+++ payment/views.py
@@ -40,3 +40,21 @@ class drivertxhistory(generics.GenericAPIView):
         )  # queryset
         serializer = self.serializer_class(transactions, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class driverearnings(generics.GenericAPIView):
+    serializer_class = DriverEarnings
+    permission_classes = [IsAuthenticated]
+
+    def get(
+        self,
+        request,
+        period,
+    ):
+        user = request.user
+        date = get_filter_date(period)
+        transactions = Transactions.objects.filter(
+            driver_id=user.id, creation_date=date
+        )  # queryset
+        total_earnings = sum(tx.amount for tx in transactions)
+        return Response({"total_earnings": total_earnings}, status=status.HTTP_200_OK)
